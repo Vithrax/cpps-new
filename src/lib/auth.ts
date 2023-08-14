ee7050bc15004d62d2f4cc4e3102e3ac459e8fc8
Vithrax <adrian.gajdulewicz@outlook.com>
2023-08-14 18:24:05 +0200

    init commit

---
+++ src/lib/auth.ts
@@ -0,0 +1,37 @@
+import { NextAuthOptions, getServerSession } from "next-auth";
+import { db } from "./db";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import GoogleProvider from "next-auth/providers/google";
+
+export const authOptions: NextAuthOptions = {
+  adapter: PrismaAdapter(db),
+  session: {
+    strategy: "jwt",
+  },
+  pages: {
+    signIn: "/sign-in",
+  },
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    }),
+  ],
+  callbacks: {
+    async session({ token, session }) {
+      if (token) {
+        session.user.id = token.id;
+        session.user.name = token.name;
+        session.user.email = token.email;
+        session.user.image = token.picture;
+      }
+
+      return session;
+    },
+    redirect() {
+      return "/";
+    },
+  },
+};
+
+export const getAuthSession = () => getServerSession(authOptions);
